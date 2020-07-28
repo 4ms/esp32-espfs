257273b443c4514b809f8be9d3791d8fa70696d3
Dan Green <danngreen1@gmail.com>
2020-07-28 11:00:35 -0700

    Fixed project path var when building with Make

--- component.mk
+++ component.mk
@@ -2,7 +2,6 @@ COMPONENT_ADD_INCLUDEDIRS := include
 COMPONENT_PRIV_INCLUDEDIRS := src heatshrink
 COMPONENT_SRCDIRS := src heatshrink
 COMPONENT_OBJS := src/espfs.o src/espfs_vfs.o heatshrink/heatshrink_decoder.o
-
 FILES := $(shell find $(PROJECT_PATH)/$(CONFIG_ESPFS_IMAGEROOTDIR) | sed -E 's/([[:space:]])/\\\1/g')
 
 ifeq ("$(CONFIG_ESPFS_LINK_BINARY)","y")
@@ -19,7 +18,7 @@ requirements.stamp:
 	touch requirements.stamp
 
 espfs_image.bin: $(FILES) requirements.stamp
-	PROJECT_DIR=${PROJECT_DIR} python "${COMPONENT_PATH}/tools/mkespfsimage.py" "${PROJECT_DIR}/${CONFIG_ESPFS_IMAGEROOTDIR}" espfs_image.bin
+	PROJECT_DIR=${PROJECT_PATH} python "${COMPONENT_PATH}/tools/mkespfsimage.py" "${PROJECT_PATH}/${CONFIG_ESPFS_IMAGEROOTDIR}" espfs_image.bin
 
 src/espfs_image.o: src/espfs_image.c
 	$(CC) $(CFLAGS) $(CPPFLAGS) -c $< -o $@
